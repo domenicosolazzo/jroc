a7b6b4995d01813039a0b10a2020d77da0ebb468
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-07-09 10:45:36 +0200

    Adding instance folder

---
+++ instance/config.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Gives you some handy tools for debugging errors. This includes a web-based stack trace and interactive Python console for errors.
+DEBUG=os.environ.get("DEBUG", False)
+
+# This is a secret key that is used by Flask to sign cookies. It’s also used by extensions like Flask-Bcrypt.
+# You should define this in your instance folder to keep it out of version control. You can read more about instance folders in the next section.
+# This should be a complex random value.
+SECRET_KEY=os.environ.get("SECRET_KEY", "youshouldchangethis")
+
+# Basic auth (only if both username and password are set)
+if os.environ.get('BASIC_AUTH_USERNAME', None) and os.environ.get('BASIC_AUTH_PASSWORD', None):
+    BASIC_AUTH_USERNAME = os.environ.get('BASIC_AUTH_USERNAME','')
+    BASIC_AUTH_PASSWORD = os.environ.get('BASIC_AUTH_PASSWORD','')
+    # If set to True, makes the whole site require HTTP basic access authentication.
+    BASIC_AUTH_FORCE = True
