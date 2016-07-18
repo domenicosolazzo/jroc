e999a41b89c154a2f63b9a9740b4e38bb1f151f4
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-07-18 10:16:59 +0200

    Importing a class in the sparql module

--- jroc/tasks/sparql/dbpedia/__init__.py
+++ jroc/tasks/sparql/dbpedia/__init__.py
@@ -1,3 +1,3 @@
 from jroc.linkeddata.sparql.SPARQL import SPARQLAdapter
 from jroc.tasks.basic.BasicTask import BasicTask
-from EntityAnnotationTask import EntityAnnotationTask, EntityAnnotationURITask, EntityAnnotationPropertiesTask
+from EntityAnnotationTask import EntityAnnotationTask, EntityAnnotationURITask, EntityAnnotationTypesTask, EntityAnnotationPropertiesTask,EntityAnnotationThumbnailTask
