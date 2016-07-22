6c679f77b013ee66a858edb9187a9d9d34a1f006
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-07-22 18:04:35 +0200

    Add more unit tests

--- tests/jroc/ner/stanford/test_stanfordner.py
+++ tests/jroc/ner/stanford/test_stanfordner.py
@@ -57,11 +57,7 @@ class StanfordNERTaggerTestCase(unittest.TestCase):
         Test for checking if the tagger returns named entities from the stanford tree
         """
         text = self.helper_readFilename('en/article2.txt')
-        ne_entities = self.tagger.tags(raw_text=text)
-        bio_tags = self.tagger.bio_tagger(ne_entities)
-        stanford_tree = self.tagger.stanford_tree(bio_tags)
-
-        actual = self.tagger.stanford_ne(stanford_tree)
+        actual = self.tagger.getEntities(raw_text=text)
         expected = [(u'House', u'ORGANIZATION'), (u'John Boehner', u'PERSON'),
                         (u'Keystone Pipeline', u'ORGANIZATION'), (u'Obama', u'PERSON'),
                         (u'Republican House', u'ORGANIZATION'), (u'John Boehner', u'PERSON'),
@@ -77,12 +73,7 @@ class StanfordNERTaggerTestCase(unittest.TestCase):
         Test for checking if the tagger returns named entities from the stanford tree with Spanish Text
         """
         text = self.helper_readFilename('es/article1.txt')
-        self.tagger = StanfordTagger()
-        ne_entities = self.tagger.tags(raw_text=text)
-        bio_tags = self.tagger.bio_tagger(ne_entities)
-        stanford_tree = self.tagger.stanford_tree(bio_tags)
-
-        actual = self.tagger.stanford_ne(stanford_tree)
+        actual = self.tagger.getEntities(raw_text=text)
         expected = [(u'Andr\xe9 Gomes', u'PERSON'), (u'Barcelona', u'ORGANIZATION'),
                     (u'Ivan Rakitic', u'PERSON'), (u'Miquel Sors', u'PERSON'), (u'Arturo Canales', u'PERSON'),
                     (u'Ivan Rakitic', u'PERSON'), (u'Barcelona', u'ORGANIZATION'), (u'Luis Enrique', u'PERSON'),
@@ -95,17 +86,11 @@ class StanfordNERTaggerTestCase(unittest.TestCase):
         Test for checking if the tagger returns named entities from the stanford tree with Norwegian Text
         """
         text = self.helper_readFilename('no/article11.txt')
-        self.tagger = StanfordTagger()
-        ne_entities = self.tagger.tags(raw_text=text)
-        bio_tags = self.tagger.bio_tagger(ne_entities)
-        stanford_tree = self.tagger.stanford_tree(bio_tags)
-
-        actual = self.tagger.stanford_ne(stanford_tree)
+        actual = self.tagger.getEntities(raw_text=text)
         expected = [(u'Jan Solberg', u'PERSON'), (u'Carl Otto S\xe6tre', u'PERSON'),
                     (u'Solberg', u'PERSON'), (u'Solberg', u'PERSON'), (u'Solberg', u'PERSON'),
                     (u'Hotell Service', u'ORGANIZATION'), (u'Knut Solberg', u'PERSON'),
                     (u'Unni og Finn Ove Carlsen', u'PERSON')]
-        print(actual)
 
         self.assertEqual(expected, actual)
 
