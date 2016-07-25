0cf2c05f6f907e7c38755140c7c43db55dac0be4
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-07-25 09:09:00 +0200

    Adding more unit tests for the stanford NER module

--- tests/jroc/ner/stanford/test_stanfordner.py
+++ tests/jroc/ner/stanford/test_stanfordner.py
@@ -14,43 +14,6 @@ class StanfordNERTaggerTestCase(unittest.TestCase):
     def tearDown(self):
         self.tagger = None
 
-    def test_nertagger_returns_named_entities(self):
-        """
-        Test the tags method of the Stanford NER Tagger.
-        """
-        text = self.helper_readFilename('en/article2.txt')
-
-        actual = self.tagger.tags(raw_text=text)
-        self.assertTrue(isinstance(actual, list))
-        self.assertTrue(len(actual) > 0)
-        firstElement = actual[0]
-        self.assertTrue(isinstance(firstElement, tuple))
-        self.assertEqual('ORGANIZATION', firstElement[1])
-
-    def test_nertagger_returns_bio_tags(self):
-        """
-        Test for checking if the tagger returns BIO tags
-        """
-        text = self.helper_readFilename('en/article2.txt')
-        ne_entities = self.tagger.tags(raw_text=text)
-
-        actual = self.tagger.bio_tagger(ne_entities)
-        self.assertTrue(isinstance(actual, list))
-        self.assertTrue(len(actual) > 0)
-        firstElement = actual[0]
-        self.assertTrue(isinstance(firstElement, tuple))
-        self.assertEqual('B-ORGANIZATION', firstElement[1])
-
-    def test_nertagger_returns_stanford_tree(self):
-        """
-        Test for checking if the tagger returns the stanford tree
-        """
-        text = self.helper_readFilename('en/article2.txt')
-        ne_entities = self.tagger.tags(raw_text=text)
-        bio_tags = self.tagger.bio_tagger(ne_entities)
-        actual = self.tagger.stanford_tree(bio_tags)
-
-        self.assertTrue(isinstance(actual,nltk.tree.Tree))
 
     def test_nertagger_returns_stanford_ne(self):
         """
@@ -58,6 +21,7 @@ class StanfordNERTaggerTestCase(unittest.TestCase):
         """
         text = self.helper_readFilename('en/article2.txt')
         actual = self.tagger.getEntities(raw_text=text)
+        print(actual)
         expected = [(u'House', u'ORGANIZATION'), (u'John Boehner', u'PERSON'),
                         (u'Keystone Pipeline', u'ORGANIZATION'), (u'Obama', u'PERSON'),
                         (u'Republican House', u'ORGANIZATION'), (u'John Boehner', u'PERSON'),
@@ -65,7 +29,6 @@ class StanfordNERTaggerTestCase(unittest.TestCase):
                         (u'America', u'LOCATION'), (u'United States', u'LOCATION'), (u'Keystone Pipeline', u'ORGANIZATION'),
                         (u'Boehner', u'PERSON'), (u'State Department', u'ORGANIZATION'), (u'GOP', u'ORGANIZATION'),
                         (u'Obama', u'PERSON'), (u'White House', u'ORGANIZATION')]
-
         self.assertEqual(expected, actual)
 
     def test_nertagger_returns_stanford_ne_spanish_text(self):
@@ -91,6 +54,23 @@ class StanfordNERTaggerTestCase(unittest.TestCase):
                     (u'Solberg', u'PERSON'), (u'Solberg', u'PERSON'), (u'Solberg', u'PERSON'),
                     (u'Hotell Service', u'ORGANIZATION'), (u'Knut Solberg', u'PERSON'),
                     (u'Unni og Finn Ove Carlsen', u'PERSON')]
+
+        self.assertEqual(expected, actual)
+
+
+    def test_nertagger_returns_stanford_ne_spanish_text(self):
+        """
+        Test for checking if the tagger returns named entities from the stanford tree with Spanish Text
+        """
+        text = self.helper_readFilename('es/article1.txt')
+        actual = self.tagger.getEntities(raw_text=text)
+        print(actual)
+        expected = [
+                    (u'Andr\xe9 Gomes', u'PERSON'), (u'Barcelona', u'ORGANIZATION'),
+                    (u'Ivan Rakitic', u'PERSON'), (u'Miquel Sors', u'PERSON'),
+                    (u'Arturo Canales', u'PERSON'), (u'Ivan Rakitic', u'PERSON'),
+                    (u'Barcelona', u'ORGANIZATION'), (u'Luis Enrique', u'PERSON'),
+                    (u'Miquel Sors', u'PERSON'), (u'Andr\xe9 Gomes', u'PERSON')]
 
         self.assertEqual(expected, actual)
 
